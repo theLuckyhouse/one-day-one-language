5f8bbd081ea6aee02ea7583f2fdb4bc222c2b4bd
theLuckyhouse <drluckyhouse12@gmail.com>
2024-07-24 16:21:07 -0400

    Tipos de Datos

---
+++ Dia 1 #Python/python.py
@@ -0,0 +1,43 @@
+'''
+Tipos de datos base
+'''
+'''
+Datos de Salida 
+Son los datos que mostramos en pantalla 
+'''
+print("Hola Mundo")
+'''
+Datos de entrada
+Son los datos que ingresa e usuario  
+'''
+datoEndrada = input()
+
+print(datoEndrada)
+
+'''
+Se utilizan para la creacion de interfacez basicas
+'''
+print("> Igresar Nombre de usuario")
+userName = input()
+print("Hola ", userName)
+
+'''
+Tipos de Datos 
+'''
+'''
+Tipo de dato Numerico 
+Son los datos que almacen datos numericos 
+pueden ser:
+'''
+
+#Tipo Entero
+myint = 3 #Son numeros enteros , sin decimales
+print(myint)
+
+#Tipo Flotante 
+myFloat = 2.2 #Son numeros decimales 
+print(myFloat)
+
+#Tipo Booleano 
+myBool = True #Este tipo de dato solo puede ser True o False
+print(myBool)
